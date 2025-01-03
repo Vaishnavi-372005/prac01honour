f721655bf97e9b9f5c97dcd4b5e54a5b605827f5
Vaishnavi-372005 <vaishnavialsundekar@gmail.com>
2025-01-03 16:06:02 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,6 +1,8 @@
-year = 2024
+year = 2025
 if(year % 4 == 0):
   print(year ," is a leap year")
 else:
  print(year, " is not a leap year")
 
+print("code run successfully")
+print("we successfully done our first practical of Honour")
